8bd2f4549343cc738835c71796c2d20e011271e1
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-25 13:05:20 -0400

    added user age and returns the year born in

--- exercise3.rb
+++ exercise3.rb
@@ -2,4 +2,10 @@ puts "what is your name"
 
 name = gets.chomp
 
-puts "Hi #{name}"
+puts "how old are you"
+
+age = gets.chomp.to_i
+
+year = 2014 - age
+
+puts "Hi #{name}. You were born in #{year}"
