343dfdcade46dd32bb99edb0628ef500ed7f235a
zap-rowsdower <brock.whitbread@gmail.com>
2014-03-25 13:25:55 -0400

    multiples of 3 and 5 now replaced by BitMaker

--- exercise4.rb
+++ exercise4.rb
@@ -2,11 +2,14 @@ one_to_hundred = (1..100)
 
 one_to_hundred.each do |i|
 	
-	if i % 3 == 0
-		puts "Bit"
+	if i % 3 == 0 && i % 5 == 0
+		puts "Bitmaker"
 	elsif i % 5 == 0
 		puts "Maker"
+	elsif i % 3 == 0
+		puts "Bit"
 	else
 		puts i
+	end
 end
-end
+
